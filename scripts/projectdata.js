43e20e24a5518d40240b286364d99f207c7857e5
Maddie <mmow313@gmail.com>
2016-03-08 13:46:06 -0800

    Resubmit day1

--- scripts/projectdata.js
+++ scripts/projectdata.js
@@ -1,7 +1,7 @@
 var rawData = [
   {
     projectTitle:'Hope I Get This Right',
-    projectCategory:"Movie",
+    projectCategory:'Movie',
     publishedDate: '2014-02-01',
     projectURL:'http://www.imdb.com/',
     body:'</p>I really had nothing to do with IMDB. This is practice</p><p>AHH</p><p>BO</p>',
@@ -9,7 +9,7 @@ var rawData = [
   },
   {
     projectTitle: 'Take Two',
-    projectCategory:"Sports",
+    projectCategory:'Sports',
     publishedDate: '2015-11-10',
     projectURL: 'http://www.espn.com/',
     body:'<p>I had nothing to do with the production of this website.</p><p> Go Warriors</p><p>le</p>',
