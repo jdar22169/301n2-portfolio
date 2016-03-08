6152c041199b796544adf1ae831a049963950c13
Maddie <mmow313@gmail.com>
2016-03-08 12:22:13 -0800

    pleasework

---
+++ scripts/projectView.js
@@ -0,0 +1,27 @@
+var projectView={};
+
+projectView.navBar = function() {
+  $('#nav-bar').on("click", 'li', function() {
+    $(".tab-content").hide();
+    $("#" + $(this).data('content')).fadeIn();
+  });
+
+  $('#nav-bar.tab:first').click();
+};
+
+projectView.setTeasers = function() {
+  $('.projectDescription *:nth-of-type(n+2)').hide();
+  $('#projects').on("click", function(event) {
+    var $eventTarget = $(event.target);
+    event.preventDefault();
+    if ($eventTarget.hasClass("more")){
+    $eventTarget.prev().children().fadeIn(1000);
+    $eventTarget.hide();
+    };
+    })
+  };
+
+$(document).ready(function(){
+  projectView.navBar();
+  projectView.setTeasers();
+});
