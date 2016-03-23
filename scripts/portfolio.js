0885db9b84009f0fbb1a03c08905e50bfa6eac50
Maddie <mmow313@gmail.com>
2016-03-22 21:02:46 -0700

    reduce function

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,44 +1,55 @@
-function Project (proj) {
-  this.projectTitle = proj.projectTitle;
-  this.projectCategory = proj.projectCategory;
-  this.publishedDate = proj.publishedDate;
-  this.projectURL = proj.projectURL;
-  this.body = proj.body;
-};
+(function(module) {
+  function Project (proj) {
+    this.projectTitle = proj.projectTitle;
+    this.projectCategory = proj.projectCategory;
+    this.publishedDate = proj.publishedDate;
+    this.projectURL = proj.projectURL;
+    this.body = proj.body;
+  };
 
-Project.all= [];
+  Project.all= [];
 
-Project.prototype.toHtml= function() {
-  var template = Handlebars.compile($('#projectsTemplate').html());
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedDate))/60/60/24/1000);
-  this.publishStatus = this.publishedDate ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  return template(this);
+  Project.prototype.toHtml= function() {
+    var template = Handlebars.compile($('#projectsTemplate').html());
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedDate))/60/60/24/1000);
+    this.publishStatus = this.publishedDate ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    return template(this);
 
-};
+  };
 
-Project.loadAll = function(projectData) {
+  Project.loadAll = function(projectData) {
 
-  projectData.sort(function(a,b) {
-    return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
-  });
-
-  projectData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-
-};
-
-
-
-Project.fetchAll = function() {
-  if (localStorage.projectData) {
-    Project.loadAll(JSON.parse(localStorage.projectData));
-    projectView.initIndexPage();
-  } else {
-    $.getJSON('scripts/projectData.json', function(data) {
-      Project.loadAll(data);
-      localStorage.setItem('projectData', JSON.stringify(data));
-      projectView.initIndexPage();
+    projectData.sort(function(a,b) {
+      return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
     });
-  }
-};
+
+    Project.all= projectData.map(function(ele) {
+      return new Project(ele);
+    });
+
+  };
+
+
+
+  Project.fetchAll = function() {
+    if (localStorage.projectData) {
+      Project.loadAll(JSON.parse(localStorage.projectData));
+      projectView.initIndexPage();
+    } else {
+      $.getJSON('../data/projectData.json', function(projectData) {
+        Project.loadAll(projectData);
+        localStorage.setItem('projectData', JSON.stringify(projectData));
+        projectView.initIndexPage();
+      });
+    }
+  };
+
+  Project.numProjects = function() {
+    return Project.all.map(function(project){
+      return (new Date(project.publishedDate)).getFullYear();
+
+    });
+  };
+
+  module.Project= Project;
+})(window);
